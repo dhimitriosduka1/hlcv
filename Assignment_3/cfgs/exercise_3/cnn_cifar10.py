6318e99f2ff369774066d5c6d792d5126f3d4c9e
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2024-06-01 00:42:05 +0200

    Implement Question 2a

--- Assignment_3/cfgs/exercise_3/cnn_cifar10.py
+++ Assignment_3/cfgs/exercise_3/cnn_cifar10.py
@@ -8,61 +8,54 @@ from src.trainers.cnn_trainer import CNNTrainer
 from src.models.cnn.model import ConvNet
 from src.models.cnn.metric import TopKAccuracy
 
+from copy import deepcopy
+
 q1_experiment = dict(
-    name = 'CIFAR10_CNN',
-
-    model_arch = ConvNet,
-    model_args = dict(
-        input_size = 3,
-        num_classes = 10,
-        hidden_layers = [128, 512, 512, 512, 512, 512],
-        activation = nn.ReLU,
-        norm_layer = nn.Identity,
-        drop_prob = 0.0,
+    name="CIFAR10_CNN",
+    model_arch=ConvNet,
+    model_args=dict(
+        input_size=3,
+        num_classes=10,
+        hidden_layers=[128, 512, 512, 512, 512, 512],
+        activation=nn.ReLU,
+        norm_layer=nn.Identity,
+        drop_prob=0.0,
     ),
-
-    datamodule = CIFAR10DataModule,
-    data_args = dict(
-        data_dir = "data/exercise-2", # You may need to change this for Colab.
-        transform_preset = 'CIFAR10',
-        batch_size = 200,
-        shuffle = True,
-        heldout_split = 0.1,
-        num_workers = 6,
+    datamodule=CIFAR10DataModule,
+    data_args=dict(
+        data_dir="data/exercise-2",  # You may need to change this for Colab.
+        transform_preset="CIFAR10",
+        batch_size=200,
+        shuffle=True,
+        heldout_split=0.1,
+        num_workers=6,
     ),
-
-    optimizer = partial(
+    optimizer=partial(
         torch.optim.Adam,
-        lr=0.002, weight_decay=0.001, amsgrad=True,
+        lr=0.002,
+        weight_decay=0.001,
+        amsgrad=True,
     ),
-    lr_scheduler = partial(
-        torch.optim.lr_scheduler.StepLR,
-        step_size=5, gamma=0.8
-    ),
-
-    criterion = nn.CrossEntropyLoss,
-    criterion_args = dict(),
-
+    lr_scheduler=partial(torch.optim.lr_scheduler.StepLR, step_size=5, gamma=0.8),
+    criterion=nn.CrossEntropyLoss,
+    criterion_args=dict(),
     metrics=dict(
-        top1 = TopKAccuracy(k=1),
-        top5 = TopKAccuracy(k=5),
+        top1=TopKAccuracy(k=1),
+        top5=TopKAccuracy(k=5),
     ),
-
-    trainer_module = CNNTrainer,
-    trainer_config = dict(
-        n_gpu = 1,
-        epochs = 10,
-        eval_period = 1,
-        save_dir = "Saved",
-        save_period = 10,
-        monitor = "off",
-        early_stop = 0,
-
-        log_step = 100,
+    trainer_module=CNNTrainer,
+    trainer_config=dict(
+        n_gpu=1,
+        epochs=10,
+        eval_period=1,
+        save_dir="Saved",
+        save_period=10,
+        monitor="off",
+        early_stop=0,
+        log_step=100,
         tensorboard=True,
         wandb=False,
     ),
-
 )
 
 
@@ -73,7 +66,8 @@ q1_experiment = dict(
 #  But make sure you have a separate config for every question so   #
 #  that we can use them for grading the assignment.                 #
 #####################################################################
-q2a_normalization_experiment = ()
+q2a_normalization_experiment = deepcopy(q1_experiment)
+q2a_normalization_experiment["model_args"]["norm_layer"] = nn.BatchNorm2d
 
 q2c_earlystop_experiment = ()
 
