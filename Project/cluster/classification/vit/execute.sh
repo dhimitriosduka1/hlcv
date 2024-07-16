e229c5726d88014a36a17693d84968abb896a871
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2024-07-16 11:10:53 +0200

    Cluster Again?

--- Project/cluster/classification/vit/execute.sh
+++ Project/cluster/classification/vit/execute.sh
@@ -3,7 +3,7 @@
 # Path to the `.py` file you want to run
 PYTHON_SCRIPT_PATH="/home/hlcv_team015/teamN/gpu_instructions/"
 # Path to the Python binary of the conda environment
-CONDA_PYTHON_BINARY_PATH="/home/hlcv_team015/teamN/miniconda3/envs/hlcv-ss23/bin/python"
+CONDA_PYTHON_BINARY_PATH="/home/hlcv_team015/teamN/miniconda3/envs/hlcv-ss24/bin/python"
 
 cd $PYTHON_SCRIPT_PATH
 $CONDA_PYTHON_BINARY_PATH "$@"
