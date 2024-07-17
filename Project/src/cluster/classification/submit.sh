7bf24d473ddbc57dc92ad5cd7ea5c110d254d05f
hlcv_team015 hlcv_team015 <hlcv_team015@conduit.hpc.uni-saarland.de>
2024-07-17 18:09:21 +0200

    WIP

--- Project/src/cluster/classification/submit.sh
+++ Project/src/cluster/classification/submit.sh
@@ -3,7 +3,7 @@
 # Path to the `.py` file you want to run
 PYTHON_SCRIPT_PATH="/home/hlcv_team015/hlcv/Project/src/cluster/classification/"
 # Path to the Python binary of the conda environment
-CONDA_PYTHON_BINARY_PATH="/home/hlcv_team015/teamN/miniconda3/envs/hlcv/bin/python"
+CONDA_PYTHON_BINARY_PATH="/home/hlcv_team015/miniconda3/envs/hlcv/bin/python"
 
 cd $PYTHON_SCRIPT_PATH
 $CONDA_PYTHON_BINARY_PATH "$@"
