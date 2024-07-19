8a177888b36fde9397da56ddd52c04f71f4b2bf0
Dhimitrios Duka <dhimitrios.duka1@gmail.com>
2024-07-19 21:16:56 +0200

    Final changes

--- Project/src/cluster/classification/submit.sh
+++ Project/src/cluster/classification/submit.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Path to the `.py` file you want to run
-PYTHON_SCRIPT_PATH="/home/hlcv_team015/hlcv/Project/src/cluster/classification/"
+PYTHON_SCRIPT_PATH="/home/hlcv_team015/hlcv/Project/src/common/"
 # Path to the Python binary of the conda environment
 CONDA_PYTHON_BINARY_PATH="/home/hlcv_team015/miniconda3/envs/hlcv/bin/python"
 
